cf7443ae1030c40ece412dd328675540aed202c6
Manuel Rucci <ruccimanuel7@gmail.com>
2021-11-12 20:58:35 +0100

    example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import cv2
 import numpy as np
+import pickle 
 
 def rotate(image, angle, center=None, scale=1.0):
     # grab the dimensions of the image
@@ -34,42 +35,70 @@ if (cap.isOpened()== False):
 
 model_face = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
 
+# Load mask classifier
+with open("models/model-aug.pkl", 'rb') as f:
+  model_mask = pickle.load(f)
+
+class_names = ["mask", "no-mask"]
+
 while(cap.isOpened()):
 
   # lettura immagine
-  ret, frame = cap.read()
+  ret, frame = cap.read()  #BGR
   
   # coversione immagine da BGR a RGB
-  #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
   #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
   
   # Rotazione dell'immagine
   #frame = rotate(frame, -90)
 
   # Trova tutte le facce nell'immagine
-  faces = model_face.detectMultiScale(frame,scaleFactor=1.2,minNeighbors=4, flags=cv2.CASCADE_DO_ROUGH_SEARCH | cv2.CASCADE_SCALE_IMAGE)
+  faces = model_face.detectMultiScale(frame,scaleFactor=1.8,minNeighbors=4, flags=cv2.CASCADE_DO_ROUGH_SEARCH | cv2.CASCADE_SCALE_IMAGE)
   
   # Trova la faccia piu grande (area piu grande)
-  ret, facebig = findLargestBB(faces)
+  #ret, facebig = findLargestBB(faces)
 
+  for (x,y,w,h) in faces:
   # Per ogni faccia fai qualcosa
-  if ret == True:
+  #if ret == True:
 
     # Extra coordiante of largest image
-    x,y,w,h = facebig
+    #x,y,w,h = facebig
     
     # Crop image 
     roi = frame[y:y+h,x:x+w]
+
+    # -----
+    # 64x64x3 --> messe su una linea
+    roi = cv2.resize(roi,(64,64))
+    inpX = roi.reshape(1,-1)
+    y_hat = model_mask.predict_proba(inpX)
+    idx = np.argmax(y_hat)
+    cv2.putText(frame, class_names[idx] + " " +  str(np.round(y_hat[0][idx])), (50, 50),  cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+   
+    #print(y_hat)
+    #print(int(y_hat))
+    #y_hat = model_mask.predict(inpX)
+    #cv2.putText(frame, class_names[int(y_hat[0])], (50, 50),  cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+  
+    #print(y_hat)
+    # -----
+    
+    
 
     # Disegna il quadrato
     cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
     # Solo sul vostro pc
-    cv2.imshow("Roi", roi)
+    #cv2.imshow("Roi", roi)
+
+  frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
   cv2.imshow("Image", frame)
   cv2.waitKey(33)
 
     # In colab o jupyter
+
     #frame = cv2.resize(frame, (128,128))
     #im_pil = Image.fromarray(roi)
     #display(im_pil)
